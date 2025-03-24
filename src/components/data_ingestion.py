413d46e70c8bfde0f5be437105bd0b4dca2e3166
Vinayak164000 <vinayakvijay2003@gmail.com>
2025-03-24 23:21:54 +0530

    model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,6 +22,26 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingeston method or component")
         try:
-            df = pd.read_csv('')
-        except:
-            pass
+            df = pd.read_csv('notebook\kag_risk_factors_cervical_cancer.csv')
+            logging.info("Read the dataset as frame")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok= True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header= True)
+            logging.info("train test split initiated")
+
+            train_set, test_set = train_test_split(df, test_size= 0.2, random_state= 42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index= False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index= False, header = True)
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomExceptions(e, sys)
+        
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
